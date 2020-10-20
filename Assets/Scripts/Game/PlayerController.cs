e9cc656546f439d7a09ca1a5fc4ea6e5a5181777
Iker <ikernoya4@gmail.com>
2020-10-20 10:15:21 -0300

    Completed basic player movement

--- Assets/Scripts/Game/PlayerController.cs
+++ Assets/Scripts/Game/PlayerController.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
-    void Start()
-    {
-        
-    }
-
+    [SerializeField] float speed;
+    [Space]
+    [SerializeField] Vector3 InitialPos;
+    Vector3 movement;
 
     void Update()
     {
-        
+        movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += movement * speed * Time.deltaTime;
     }
 }
