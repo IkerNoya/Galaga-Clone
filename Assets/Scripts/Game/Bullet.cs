5d069e4172cc8c85a3ea56976b4dfc7d0ba0fe16
Iker <ikernoya4@gmail.com>
2020-10-20 11:11:29 -0300

    Added shooting mechanics

---
+++ Assets/Scripts/Game/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] float speed;
+    Vector3 direction;
+    void Start()
+    {
+        direction = new Vector3(0, 1, 0);
+        Destroy(gameObject, 1.5f);
+    }
+
+    void Update()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+    }
+}
--- Assets/Scripts/Game/PlayerController.cs
+++ Assets/Scripts/Game/PlayerController.cs
@@ -7,11 +7,31 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float speed;
     [Space]
     [SerializeField] Vector3 InitialPos;
+    [Space]
+    [SerializeField] GameObject bullet;
+    [SerializeField] KeyCode FireButton;
+    [Space]
+    [SerializeField] GameObject gunLeft;
+    [SerializeField] GameObject gunRight;
     Vector3 movement;
 
     void Update()
     {
         movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
         transform.position += movement * speed * Time.deltaTime;
+        Inputs();
+    }
+    void Inputs()
+    {
+        if (Input.GetKeyDown(FireButton))
+        {
+            if (bullet != null)
+            {
+                if(gunLeft!=null)
+                    Instantiate(bullet, gunLeft.transform.position, Quaternion.identity);
+                if(gunRight!=null)
+                    Instantiate(bullet, gunRight.transform.position, Quaternion.identity);
+            }
+        }
     }
 }
