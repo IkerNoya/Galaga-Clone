812b99739e5e14738ced0c7c3a825e18b5587352
Iker <ikernoya4@gmail.com>
2020-12-15 08:44:23 -0300

    Added a level selector in menu

--- Assets/Scripts/UI/UIMenu.cs
+++ Assets/Scripts/UI/UIMenu.cs
@@ -3,6 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class UIMenu : MonoBehaviour
 {
+    [SerializeField] GameObject levelSelector;
+    [SerializeField] GameObject playButton;
+    [SerializeField] GameObject levelsText;
+    public void OnClickPlay()
+    {
+        if(levelSelector!=null) levelSelector.SetActive(true);
+        if(playButton != null) playButton.SetActive(false);
+        if(levelsText != null) levelsText.SetActive(true);
+    }
     public void ChangeScene(string name)
     {
         SceneManager.LoadScene(name);
